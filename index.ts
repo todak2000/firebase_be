6e02f255b8133db7dfae2f116cb9e59f60b5ff3b
Daniel Olagunju <todak2000@gmail.com>
2023-12-29 14:19:49 +0100

    completed1

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ export const app: Application = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
-app.use(express.static("public"));
+app.use(express.static("dist"));
 
 swagger(app);
 app.use("/", router);
